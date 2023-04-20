03bc0afd5fa7108d3e4636330a407ce478b4f75e
H. Ozgan <hozgan@gmail.com>
2023-04-20 17:58:00 +0100

    added unit test for service

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -4,36 +4,19 @@ namespace App\Services;
 use App\Models\User;
 
 class UserService {
- 
-    public function store(array $userData): User
-    {
-        $user = User::create($userData);
- 
-        $user->roles()->sync($userData['roles']);
- 
-        // More actions with that user: let's say, 5+ more lines of code
-        // - Upload avatar
-        // - Email to the user
-        // - Notify admins about new user
-        // - Create some data for that user
-        // - and more...
- 
-        return $user;
-    }
- 
-    public function update(array $userData, User $user): User
-    {
-        $user->update($userData);
-        $user->roles()->sync($userData['roles']);
- 
-        // Also, more actions with that user
-        return $user;
+    protected User $user;
+
+    /**
+     * @param User $user
+     */
+    public function __construct(User $user) {
+        $this->user = $user;
     }
 
     /** Return Types
      * @return \Illuminate\Database\Eloquent\Collection<int, static>
      */
     public function getAll() {
-        return User::all();
+        return $this->user::all();
     }
 }
---
+++ tests/Unit/Services/UserServiceTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use App\Models\User;
+use App\Services\UserService;
+use PHPUnit\Framework\TestCase;
+use Mockery;
+
+class UserServiceTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_should_call_user_model(): void
+    {
+        $mockUser = Mockery::mock(User::class);
+        $mockUser
+            ->shouldReceive('all')
+            ->once()
+            ->andReturn([[
+                'id' => 12,
+                'name' => 'Hermione Granger',
+                'email' => 'hermionegranger@hogwarts.com'
+            ]]);
+
+        $userService = new UserService($mockUser);
+        $users = $userService->getAll();
+
+        expect($users)->toHaveLength(1);
+        expect($users[0])->toHaveKey('name', 'Hermione Granger');
+        expect($users[0])->toHaveKey('email', 'hermionegranger@hogwarts.com');
+    }
+}
